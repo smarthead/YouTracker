e76af8f25b2cea6c23a25a6fa4579397d56ceb9b
Ramil Aminov <raminov@smarthead.ru>
2019-05-16 23:14:59 +0300

    Add electron integration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const process = window.require('process');
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello World!</h1>       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Electron version: {process.versions.electron}.
         </p>
         <a
           className="App-link"
