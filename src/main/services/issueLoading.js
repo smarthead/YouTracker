71f9ae3fb717e880785df339f52f1daec8fd450d
Аминов Рамиль <raminov@smarthead.ru>
2019-05-23 09:13:35 +0200

    Получение и отображение затраченного по задаче времени

--- src/main/services/issueLoading.js
+++ src/main/services/issueLoading.js
@@ -1,15 +1,19 @@
 import { EventEmitter } from 'events';
 import api from './api';
+import parseIssues from './parseIssues';
 
 let events = new EventEmitter();
 
 let issues = [];
 
 const reload = () => {
-  api.getIssues().then((result) => {
-    issues = result;
-    events.emit('changed', issues);
-  });
+  api.getIssues()
+    .then(parseIssues)
+    .then((result) => {
+      console.log(result);
+      issues = result;
+      events.emit('changed', issues);
+    });
 };
 
 const getIssues = () => issues;
--- src/main/urls.js
+++ src/main/urls.js
@@ -1,7 +1,7 @@
 const base = 'https://ramilaminov.myjetbrains.com/youtrack';
 
 // API
-const getIssues = `${base}/api/issues?fields=id,idReadable,summary&query=Assignee:me`;
+const getIssues = `${base}/api/issues?fields=id,idReadable,summary,customFields(id,projectCustomField(id,field(id,name)),value(id,minutes,presentation))&query=Assignee:me`;
 const postWorkItems = (issueId) => `${base}/api/issues/${issueId}/timeTracking/workItems`;
 
 // Web
