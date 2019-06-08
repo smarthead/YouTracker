7b4237fca9080e4775392b62107cc3fe0f3b6cd0
Ramil Aminov <raminov@smarthead.ru>
2019-06-08 19:14:58 +0300

    Изменен запрос задач

--- src/main/services/urls.js
+++ src/main/services/urls.js
@@ -1,12 +1,17 @@
 const base = 'https://ramilaminov.myjetbrains.com';
 
 // API
+
 const oauth = `${base}/hub/api/rest/oauth2/token`;
 const getMe = `${base}/youtrack/api/admin/users/me?fields=id,login`;
-const getIssues = `${base}/youtrack/api/issues?fields=id,idReadable,summary,customFields(id,projectCustomField(id,field(id,name)),value(id,minutes,presentation))&query=Assignee:me`;
+
+// for: me #Unresolved sort by: created desc 
+const getIssues = `${base}/youtrack/api/issues?fields=id,idReadable,summary,customFields(id,projectCustomField(id,field(id,name)),value(id,minutes,presentation))&query=for:%20me%20%23Unresolved%20sort%20by:%20created%20desc`;
+
 const postWorkItems = (issueId) => `${base}/youtrack/api/issues/${issueId}/timeTracking/workItems`;
 
 // Web
+
 const viewIssue = (idReadable) => `${base}/youtrack/issue/${idReadable}`;
 
 export default { oauth, getMe, getIssues, postWorkItems, viewIssue };
