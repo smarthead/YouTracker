2be73fc51c2e469b1901cd9175b49f18ddf07bfd
Ramil Aminov <raminov@smarthead.ru>
2019-07-28 21:26:46 +0300

    Очистка поля поиска кнопкой и Escape

--- src/renderer/components/SearchBar.js
+++ src/renderer/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const SearchBar = (props) => {
     const { query, onQueryChange } = props;
@@ -6,6 +6,21 @@ const SearchBar = (props) => {
     const handleQueryChange = (event) => {
         onQueryChange(event.target.value);
     }
+
+    const handleClearClick = (event) => {
+        onQueryChange('');
+    };
+    
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'Escape') {
+                onQueryChange('');
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [onQueryChange]);
 
     return (
         <div className="searchBar">
@@ -16,6 +31,13 @@ const SearchBar = (props) => {
                 value={query}
                 onChange={handleQueryChange}
             />
+            <button
+                className="searchBar__clear-button"
+                onClick={handleClearClick}
+                hidden={query === ''}
+            >
+                <i className="fas fa-times" />
+            </button>
         </div>
     );
 };
