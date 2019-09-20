7161feff12756a8f4e03403f2ecdd019ee9611ea
Ramil Aminov <raminov@smarthead.ru>
2019-09-20 22:07:09 +0300

    Исправлен запуск webpack на windows

--- webpack.nodeloader.js
+++ webpack.nodeloader.js
@@ -3,9 +3,11 @@ const path = require('path');
 module.exports = function(content) {
     this.addDependency(this.resourcePath);
 
-    const relativePath = path.relative(__dirname, this.resourcePath);
+    const relativePath = path.relative(__dirname, this.resourcePath)
+        .split(path.sep)
+        .join('/');
 
-    return (`
+    return `
         const path = require('path');
         const { app } = require('electron');
         const filePath = path.resolve(app.getAppPath(), '${relativePath}');
@@ -14,7 +16,7 @@ module.exports = function(content) {
         } catch (e) {
             throw new Error('Cannot open ' + filePath + ': ' + e);
         }
-    `);
+    `;
 };
 
 module.exports.raw = true;
